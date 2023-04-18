1f6f05199afffe0b23a5a48ec1a1eaf046800492
Jakub Senko <m@jsenko.net>
2023-04-18 15:47:16 +0200

    chore: prepare migration from Sentry to GlitchTip

---
+++ api/src/main/java/io/apicurio/tenantmanager/TemporaryOnReadySentryEventEmitter.java
@@ -0,0 +1,27 @@
+package io.apicurio.tenantmanager;
+
+import io.quarkus.scheduler.Scheduled;
+import io.sentry.Sentry;
+import org.slf4j.Logger;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+
+/**
+ * TODO: Remove me, or even better, move me to io.apicurio.common.apps.logging.sentry.AbstractSentryConfiguration
+ * <p>
+ * Emits a Sentry event on startup, to ensure the GlitchTip integration is working.
+ */
+@ApplicationScoped
+public class TemporaryOnReadySentryEventEmitter {
+
+    @Inject
+    Logger log;
+
+    @Scheduled(every = "P1000D", delayed = "45s")
+    void onReady() {
+        var message = "Sentry enabled for Tenant Manager";
+        log.info("Trying to send a Sentry event: {}", message);
+        Sentry.capture(message);
+    }
+}
